08128ca33cfe38931d3565752f9afbe7b2013be0
Pritam <pritsaccount@gmail.com>
2024-05-16 17:15:09 +0530

    top navbar made nearly similar to original;

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,7 +6,7 @@ import IndiaFlag from "./StaticImages/Indiaflag.png";
 function Navbar() {
     return (
         <header class="blue-bg">
-            <nav class="">
+            <nav class="top-nav">
                 <div class="container d-flex">
                     <div class="top-nav-left d-flex">
                         <div class="logo-holder">
@@ -22,29 +22,35 @@ function Navbar() {
                         <div class="nav-item-custom">
                             INR
                         </div>
-                        <div class="nav-item-custom">
+                        <div class="">
                             <div class="navbar-picture">
                                 <img src={IndiaFlag} alt=""></img>
                             </div>
                         </div>
-                        <div class="nav-item-custom">
-                        <div class="questionmark-holder">
-                            <a href="#">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff">
-                                    <path d="M9.75 9a2.25 2.25 0 1 1 3 2.122 2.25 2.25 0 0 0-1.5 2.122v1.006a.75.75 0 0 0 1.5 0v-1.006c0-.318.2-.602.5-.708A3.75 3.75 0 1 0 8.25 9a.75.75 0 1 0 1.5 0M12 16.5a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5M22.5 12c0 5.799-4.701 10.5-10.5 10.5S1.5 17.799 1.5 12 6.201 1.5 12 1.5 22.5 6.201 22.5 12m1.5 0c0-6.627-5.373-12-12-12S0 5.373 0 12s5.373 12 12 12 12-5.373 12-12">
-                                    </path>
-                                </svg>
-                            </a>
+                        <div class="">
+                            <div class="questionmark-holder">
+                                <a href="#">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff">
+                                        <path d="M9.75 9a2.25 2.25 0 1 1 3 2.122 2.25 2.25 0 0 0-1.5 2.122v1.006a.75.75 0 0 0 1.5 0v-1.006c0-.318.2-.602.5-.708A3.75 3.75 0 1 0 8.25 9a.75.75 0 1 0 1.5 0M12 16.5a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5M22.5 12c0 5.799-4.701 10.5-10.5 10.5S1.5 17.799 1.5 12 6.201 1.5 12 1.5 22.5 6.201 22.5 12m1.5 0c0-6.627-5.373-12-12-12S0 5.373 0 12s5.373 12 12 12 12-5.373 12-12">
+                                        </path>
+                                    </svg>
+                                </a>
                             </div>
                         </div>
                         <div class="nav-item-custom">
                             List Your Property
                         </div>
-                        <div class="nav-item-custom">
-                            Register
-                        </div>
-                        <div class="nav-item-custom">
-                            Sign In
+                        <div class="d-flex register-sign-in">
+                            <div class="nav-item-custom">
+                                <div class="navbar-button">
+                                    Register
+                                </div>
+                            </div>
+                            <div class="nav-item-custom">
+                                <div class="navbar-button">
+                                    Sign In
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
