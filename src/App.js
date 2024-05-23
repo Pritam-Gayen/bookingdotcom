013a8b587506d7c62367af3ef761e87202a49791
Pritam <pritsaccount@gmail.com>
2024-05-23 13:25:01 +0530

    heading added in Home page;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+const Home = () => {
+  return (
+    <div class="calender-holder">
+      <div class="container heading-holder">
+        <h1>Find your next stay</h1>
+        <h3>Search low prices on hotels, homes and much more...</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
