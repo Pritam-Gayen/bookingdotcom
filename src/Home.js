0291ca1f0c57add06c4673c4089d6bd2e771fd53
Pritam <pritsaccount@gmail.com>
2024-05-25 10:18:33 +0530

    Revert "added date picker; not working;"
    This reverts commit a968330c140f1d18e48d2985dcf7f178d718e8cb.

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import './Home.css';
-import DatePicker from './DatePicker';
 
 const places = [
   'New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix',
@@ -90,25 +88,6 @@ document.addEventListener('click', function (event) {
 
 
 const Home = () => {
-  const [showDatePicker, setShowDatePicker] = useState(false);
-
-  const handleClickOutside = (event) => {
-    const datePickerHolder = document.querySelector('.date-picker-holder');
-    const calendarBtn = document.getElementById('calender-btn');
-    if (datePickerHolder && !datePickerHolder.contains(event.target) && !calendarBtn.contains(event.target)) {
-      setShowDatePicker(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
-
-
-
   return (
     <div>
       <div class="calender-holder">
@@ -128,7 +107,7 @@ const Home = () => {
 
               </div>
             </div>
-            <div class="box-filling d-flex" id="calender-btn">
+            <div class="box-filling d-flex">
               <div class="box-filling-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#808080" d="M22.5 13.5v8.25a.75.75 0 0 1-.75.75H2.25a.75.75 0 0 1-.75-.75V5.25a.75.75 0 0 1 .75-.75h19.5a.75.75 0 0 1 .75.75zm1.5 0V5.25A2.25 2.25 0 0 0 21.75 3H2.25A2.25 2.25 0 0 0 0 5.25v16.5A2.25 2.25 0 0 0 2.25 24h19.5A2.25 2.25 0 0 0 24 21.75zm-23.25-3h22.5a.75.75 0 0 0 0-1.5H.75a.75.75 0 0 0 0 1.5M7.5 6V.75a.75.75 0 0 0-1.5 0V6a.75.75 0 0 0 1.5 0M18 6V.75a.75.75 0 0 0-1.5 0V6A.75.75 0 0 0 18 6M5.095 14.03a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06m.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5m-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06m.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5m5.845-3.97a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06m.53-1.28A1.125 1.125 0 1 0 12 15a1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5m-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06M12 18a1.125 1.125 0 1 0 0 2.25A1.125 1.125 0 0 0 12 18a.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5m5.845-3.97a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06m.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5m-.53 6.53a.75.75 0 1 0 1.06-1.06.75.75 0 0 0-1.06 1.06m.53-1.28a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25.75.75 0 0 0 0 1.5.375.375 0 1 1 0-.75.375.375 0 0 1 0 .75.75.75 0 0 0 0-1.5"></path></svg>
               </div>
@@ -136,11 +115,6 @@ const Home = () => {
               <span class="dash"> — </span>
               <span class="check-out-input">Check-out date</span>
             </div>
-            {showDatePicker && (
-              <div className="date-picker-holder">
-                <DatePicker />
-              </div>
-            )}
             <div class="box-filling d-flex">
               <div class="box-filling-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#808080" d="M16.5 6a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0M18 6A6 6 0 1 0 6 6a6 6 0 0 0 12 0M3 23.25a9 9 0 1 1 18 0 .75.75 0 0 0 1.5 0c0-5.799-4.701-10.5-10.5-10.5S1.5 17.451 1.5 23.25a.75.75 0 0 0 1.5 0"></path></svg>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css';
-import 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js';
-import 'jquery/dist/jquery.min.js';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
