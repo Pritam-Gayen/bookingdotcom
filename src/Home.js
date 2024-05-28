5d5b2249d16c474d476e7ace819dd26dc9729709
Pritam <pritsaccount@gmail.com>
2024-05-28 11:25:18 +0530

    removed react-date-picker and cleaned package.json;

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import './Home.css';
 
 
@@ -115,7 +113,6 @@ document.addEventListener('click', function (event) {
 
 
 const Home = () => {
-  const [startDate, setStartDate] = useState(new Date());
   return (
     <div>
       <div class="calender-holder">
@@ -144,58 +141,7 @@ const Home = () => {
               <span class="check-out-input">Check-out date</span>
             </div>
             <div id="show-calender-holder">
-              <DatePicker
-                renderCustomHeader={({
-                  monthDate,
-                  customHeaderCount,
-                  decreaseMonth,
-                  increaseMonth,
-                }) => (
-                  <div>
-                    <button
-                      aria-label="Previous Month"
-                      className={
-                        "react-datepicker__navigation react-datepicker__navigation--previous"
-                      }
-                      style={customHeaderCount === 1 ? { visibility: "hidden" } : null}
-                      onClick={decreaseMonth}
-                    >
-                      <span
-                        className={
-                          "react-datepicker__navigation-icon react-datepicker__navigation-icon--previous"
-                        }
-                      >
-                        {"<"}
-                      </span>
-                    </button>
-                    <span className="react-datepicker__current-month">
-                      {monthDate.toLocaleString("en-US", {
-                        month: "long",
-                        year: "numeric",
-                      })}
-                    </span>
-                    <button
-                      aria-label="Next Month"
-                      className={
-                        "react-datepicker__navigation react-datepicker__navigation--next"
-                      }
-                      style={customHeaderCount === 0 ? { visibility: "hidden" } : null}
-                      onClick={increaseMonth}
-                    >
-                      <span
-                        className={
-                          "react-datepicker__navigation-icon react-datepicker__navigation-icon--next"
-                        }
-                      >
-                        {">"}
-                      </span>
-                    </button>
-                  </div>
-                )}
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                monthsShown={2}
-              />
+              
             </div>
             <div class="box-filling d-flex">
               <div class="box-filling-icon">
