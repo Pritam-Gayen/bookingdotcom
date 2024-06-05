8749c38da8dcd5290428596ab8c228dda9aa9042
Pritam <pritsaccount@gmail.com>
2024-06-05 16:09:36 +0530

    five-btn color change made similar as original site

--- src/TabbedDateRangePicker.js
+++ src/TabbedDateRangePicker.js
@@ -4,31 +4,63 @@ import './TabbedDateRangePicker.css';
 
 const TabbedDateRangePicker = ({ dateRange, setDateRange }) => {
     const [activeTab, setActiveTab] = useState('calendar');
+    const [activeButton, setActiveButton] = useState("Exact dates");
+
+    const handleButtonClick = (buttonLabel) => {
+        setActiveButton(buttonLabel);
+    };
+
 
     const renderTabContent = () => {
         switch (activeTab) {
             case 'calendar':
                 return <div>
                     <DateRangePicker dateRange={dateRange} setDateRange={setDateRange} />
-                    <div class="five-btn-holder">
-                        <div class="five-btn">Exact dates</div>
-                        <div class="five-btn">
-                            <svg class="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path></svg>
+                    <div className="five-btn-holder">
+                        <div
+                            className={`five-btn ${activeButton === "Exact dates" ? "five-btn-active" : ""}`}
+                            onClick={() => handleButtonClick("Exact dates")}
+                        >
+                            Exact dates
+                        </div>
+                        <div
+                            className={`five-btn ${activeButton === "1 day" ? "five-btn-active" : ""}`}
+                            onClick={() => handleButtonClick("1 day")}
+                        >
+                            <svg className="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path>
+                            </svg>
                             <span>1 day</span>
                         </div>
-                        <div class="five-btn">
-                            <svg class="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path></svg>
+                        <div
+                            className={`five-btn ${activeButton === "2 days" ? "five-btn-active" : ""}`}
+                            onClick={() => handleButtonClick("2 days")}
+                        >
+                            <svg className="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path>
+                            </svg>
                             <span>2 days</span>
                         </div>
-                        <div class="five-btn">
-                            <svg class="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path></svg>
+                        <div
+                            className={`five-btn ${activeButton === "3 days" ? "five-btn-active" : ""}`}
+                            onClick={() => handleButtonClick("3 days")}
+                        >
+                            <svg className="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path>
+                            </svg>
                             <span>3 days</span>
                         </div>
-                        <div class="five-btn">
-                            <svg class="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path></svg>
+                        <div
+                            className={`five-btn ${activeButton === "7 days" ? "five-btn-active" : ""}`}
+                            onClick={() => handleButtonClick("7 days")}
+                        >
+                            <svg className="plus-minus" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path fill="rgb(67, 65, 65)" d="M21.14 22.94a1 1 0 0 1-1 1H3.86a1 1 0 1 1 0-2h16.28a1 1 0 0 1 1 1M4 10h7v7a1 1 0 0 0 2 0v-7h7a1 1 0 0 0 0-2h-7V1a1 1 0 0 0-2 0v7H4a1 1 0 0 0 0 2"></path>
+                            </svg>
                             <span>7 days</span>
                         </div>
                     </div>
+
                 </div>;
             case 'flexible':
                 return <div>I'm flexible content goes here</div>;
