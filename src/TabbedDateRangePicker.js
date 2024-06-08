0568a15ee96934e2d9ade1fc3c37f2bb4d1d99ac
Pritam <pritsaccount@gmail.com>
2024-06-08 15:25:51 +0530

    i'm flexible tab made; bullet portion style need to make accurate;

--- src/TabbedDateRangePicker.js
+++ src/TabbedDateRangePicker.js
@@ -1,6 +1,100 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import DateRangePicker from './DateRangePicker';
 import './TabbedDateRangePicker.css';
+
+
+const getNextMonths = () => {
+    const months = [];
+    const currentDate = new Date();
+
+    for (let i = 0; i < 12; i++) {
+        const futureDate = new Date(currentDate.getFullYear(), currentDate.getMonth() + i, 1);
+        const month = futureDate.toLocaleString('default', { month: 'short' });
+        const year = futureDate.getFullYear();
+        months.push({ month, year });
+    }
+
+    return months;
+};
+
+
+const MonthDisplay = () => {
+    const months = getNextMonths();
+    const containerRef = useRef(null);
+    const [scrollable, setScrollable] = useState({ left: false, right: true });
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (containerRef.current) {
+                const { scrollLeft, scrollWidth, clientWidth } = containerRef.current;
+                setScrollable({
+                    left: scrollLeft > 0,
+                    right: scrollLeft < scrollWidth - clientWidth
+                });
+            }
+        };
+
+        if (containerRef.current) {
+            containerRef.current.addEventListener('scroll', handleScroll);
+        }
+
+        return () => {
+            const currentRef = containerRef.current;
+            if (currentRef) {
+                currentRef.removeEventListener('scroll', handleScroll);
+            }
+        };
+    }, []);
+
+
+    const handleScrollLeft = () => {
+        if (containerRef.current) {
+            containerRef.current.scrollBy({
+                left: -(containerRef.current.clientWidth / 6),
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const handleScrollRight = () => {
+        if (containerRef.current) {
+            containerRef.current.scrollBy({
+                left: (containerRef.current.clientWidth / 6),
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    return (
+        <div className="month-display-container d-flex">
+            {scrollable.left && (
+                <button class="month-display-left" onClick={handleScrollLeft}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-rtl-flip="true"><path d="M15.087 19.236a.9.9 0 0 1-.642-.266l-6.057-6.057A1.3 1.3 0 0 1 8 11.968c-.008-.35.123-.69.364-.945l6.057-6.057a.91.91 0 0 1 1.284 0 .895.895 0 0 1 0 1.284l-5.694 5.718 5.718 5.718a.896.896 0 0 1 0 1.284.88.88 0 0 1-.642.266"></path></svg>
+                </button>
+            )}
+            <div className="months-holder d-flex" ref={containerRef}>
+
+                {months.map(({ month, year }, index) => (
+                    <div className="select-month" key={index}>
+                        <div className="month-icon-holder">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <path d="M22.502 13.5v8.25a.75.75 0 0 1-.75.75h-19.5a.75.75 0 0 1-.75-.75V5.25a.75.75 0 0 1 .75-.75h19.5a.75.75 0 0 1 .75.75zm1.5 0V5.25A2.25 2.25 0 0 0 21.752 3h-19.5a2.25 2.25 0 0 0-2.25 2.25v16.5A2.25 2.25 0 0 0 2.252 24h19.5a2.25 2.25 0 0 0 2.25-2.25zm-23.25-3h22.5a.75.75 0 0 0 0-1.5H.752a.75.75 0 0 0 0 1.5M7.502 6V.75a.75.75 0 0 0-1.5 0V6a.75.75 0 0 0 1.5 0m10.5 0V.75a.75.75 0 0 0-1.5 0V6a.75.75 0 0 0 1.5 0"></path>
+                            </svg>
+                        </div>
+                        <span className="month">{month}</span>
+                        <span className="year">{year}</span>
+                    </div>
+                ))}
+            </div>
+            {scrollable.right && (
+                <button class="month-display-right" onClick={handleScrollRight}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-rtl-flip="true"><path d="M8.913 19.236a.9.9 0 0 0 .642-.266l6.057-6.057a1.3 1.3 0 0 0 .388-.945c.008-.35-.123-.69-.364-.945L9.58 4.966a.91.91 0 0 0-1.284 0 .896.896 0 0 0 0 1.284l5.694 5.718-5.718 5.718a.896.896 0 0 0 0 1.284.88.88 0 0 0 .642.266"></path></svg>
+                </button>
+            )}
+        </div>
+    );
+};
+
 
 const TabbedDateRangePicker = ({ dateRange, setDateRange }) => {
     const [activeTab, setActiveTab] = useState('calendar');
@@ -104,44 +198,39 @@ const TabbedDateRangePicker = ({ dateRange, setDateRange }) => {
                     <div class="bullet-div">
                         <h3>How long do you want to stay?</h3>
                         <div class="d-flex">
-                                <div className="radio">
-                                    <label>
-                                        <input type="radio" value="option1" checked={true} />
-                                        A weekend
-                                    </label>
-                                </div>
-                                <div className="radio">
-                                    <label>
-                                        <input type="radio" value="option1" checked={true} />
-                                        A week
-                                    </label>
-                                </div>
-                                <div className="radio">
-                                    <label>
-                                        <input type="radio" value="option1" checked={true} />
-                                       A month
-                                    </label>
-                                </div>
-                                <div className="radio">
-                                    <label>
-                                        <input type="radio" value="option1" checked={true} />
-                                        Other
-                                    </label>
-                                </div>
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" value="option1" checked={true} />
+                                    <span class="radio-span">A weekend</span>
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" value="option1" checked={true} />
+                                    <span class="radio-span">A week</span>
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" value="option1" checked={true} />
+                                    <span class="radio-span">A month</span>
+                                </label>
+                            </div>
+                            <div className="radio">
+                                <label>
+                                    <input type="radio" value="option1" checked={true} />
+                                    <span class="radio-span">Other</span>
+                                </label>
+                            </div>
                         </div>
                     </div>
                     <div class="month-select-div">
                         <h3>When do you want to go?</h3>
                         <h4>Select up to 3 months</h4>
-                        <div class="months-holder">
-                            <div class="select-month">
-                                <div class="month-icon-holder"></div>
-                                <p>Month Name</p>
-                            </div>
-                        </div>
+                        <MonthDisplay />
                     </div>
                     <div class="btn-div d-flex">
-                        <p>Selected month</p>
+                        <span class="selected-month">Select preferred months</span>
                         <div class="select-dates-btn">Select dates</div>
                     </div>
                 </div>;
