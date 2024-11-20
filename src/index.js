82bafb1ae1853a9b8816e811bcd9c357c8098591
Pritam <pritsaccount@gmail.com>
2024-11-20 12:46:05 +0530

    tool tip added in navbar for under construction buttons;

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'rsuite/dist/rsuite.min.css';
-
+import 'react-tooltip/dist/react-tooltip.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
