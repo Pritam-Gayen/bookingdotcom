746559a8b5d283d28a096616fb4d1fd8cb5c053f
Pritam <pritsaccount@gmail.com>
2024-12-04 10:30:35 +0530

    using REACT_APP_CLIENTID

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,7 +12,7 @@ import ListYourProperty from './ListYourProperty';
 
 // import LogoutButton from './logout';
 
-const clientID = JSON.stringify(process.env.CLIENTID);
+const clientID = process.env.REACT_APP_CLIENTID;
 console.log("clientID ",clientID);
 // const clientID = '2518369344-q9dpo7qi8cf6miot94a0f4tiqhnslanc.apps.googleusercontent.com';
 
