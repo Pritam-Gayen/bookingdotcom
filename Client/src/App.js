5a98641ca3d218becb3a647be0f267f8b99852ff
Pritam <pritsaccount@gmail.com>
2024-12-04 10:20:23 +0530

    using json

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,8 +12,8 @@ import ListYourProperty from './ListYourProperty';
 
 // import LogoutButton from './logout';
 
-const clientID = process.env.CLIENTID;
-console.log(clientID);
+const clientID = JSON.stringify(process.env.CLIENTID);
+console.log("clientID ",clientID);
 // const clientID = '2518369344-q9dpo7qi8cf6miot94a0f4tiqhnslanc.apps.googleusercontent.com';
 
 function App() {
