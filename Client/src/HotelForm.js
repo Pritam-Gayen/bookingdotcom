61aa9474c9e4dd2b0821fdc9f58a9f2831b9ed46
Pritam <pritsaccount@gmail.com>
2024-12-05 15:24:01 +0530

    imported axios

--- Client/src/HotelForm.js
+++ Client/src/HotelForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import './HotelForm.css';
 const HotelForm = ({ userEmail }) => {
     const [hotelName, setHotelName] = useState('');
