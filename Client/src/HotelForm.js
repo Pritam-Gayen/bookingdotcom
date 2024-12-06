391af48e17a20c9dae9b49e4dabcf0c80cfb9944
Pritam <pritsaccount@gmail.com>
2024-12-06 16:48:36 +0530

    HotelForm modified for append data;

--- Client/src/HotelForm.js
+++ Client/src/HotelForm.js
@@ -24,30 +24,35 @@ const HotelForm = ({ userEmail }) => {
         e.preventDefault();
 
         // Handle form submission here, e.g., send data to server
+        const formData = new FormData();
 
-        // Gather data from form fields
-        // formData.append('hotel[name]', hotelName);
-        // formData.append('hotel[address]', address);
-        // formData.append('hotel[city]', city);
-        // formData.append('hotel[description]', description);
-        // formData.append('hotel[rating]', rating);
-        // formData.append('hotelwoner', userEmail);
-        const formData = {name: hotelName, address: address, city: city, description: description, rating: rating, hotelwoner: userEmail, images: images };
+        const hotelData = {
+            name: hotelName,
+            address: address,
+            city: city,
+            description: description,
+            rating: rating,
+            hotelwoner: userEmail
+        };
+
+        formData.append('hotel', JSON.stringify(hotelData));
+
         // Handle multiple image uploads
-        // if (images) {
-        //     for (let i = 0; i < images.length; i++) {
-        //         formData.append(`images[${i}]`, images[i]);
-        //     }
-        // }
+        if (images) {
+            for (let i = 0; i < images.length; i++) {
+                formData.append(`images[${i}]`, images[i]);
+            }
+        }
+
 
         console.log("formData ", formData);
         try {
-            await axios.post('https://bookingdotcom-gkgr.onrender.com/addhotel', {hotel: formData})
-            .then(res => res.data)
-            .then(data => {
-            console.log("response: ", data)
-            navigate('/edithotels');
-            });
+            await axios.post('https://bookingdotcom-gkgr.onrender.com/addhotel', { hotel: formData })
+                .then(res => res.data)
+                .then(data => {
+                    console.log("response: ", data)
+                    navigate('/edithotels');
+                });
 
             // if (response.status === 200) {
             //     console.log('Successful upload:', response.data);
@@ -56,7 +61,7 @@ const HotelForm = ({ userEmail }) => {
             // else {
             //     console.error('Unexpected response:', response.statusText); // Handle non-200 status codes
             // }
-        } 
+        }
         catch (error) {
             console.error('Upload error:', error);
         }
@@ -64,68 +69,68 @@ const HotelForm = ({ userEmail }) => {
 
 
 
-return (
-    <form onSubmit={handleSubmit}>
-        <div className='hotel-form-input'>
-            <label htmlFor="hotelName">Hotel Name:</label>
-            <input
-                type="text"
-                id="hotelName"
-                value={hotelName}
-                onChange={(e) => setHotelName(e.target.value)}
-            />
-        </div>
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className='hotel-form-input'>
+                <label htmlFor="hotelName">Hotel Name:</label>
+                <input
+                    type="text"
+                    id="hotelName"
+                    value={hotelName}
+                    onChange={(e) => setHotelName(e.target.value)}
+                />
+            </div>
 
-        <div className='hotel-form-input'>
-            <label htmlFor="address">Hotel Address:</label>
-            <input
-                type="text"
-                id="hotelAddress"
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}
-            />
-        </div>
+            <div className='hotel-form-input'>
+                <label htmlFor="address">Hotel Address:</label>
+                <input
+                    type="text"
+                    id="hotelAddress"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                />
+            </div>
 
-        <div className='hotel-form-input'>
-            <label htmlFor="city">City:</label>
-            <input
-                type="text"
-                id="city"
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
-            />
-        </div>
+            <div className='hotel-form-input'>
+                <label htmlFor="city">City:</label>
+                <input
+                    type="text"
+                    id="city"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+            </div>
 
-        <div className='hotel-form-input'>
-            <label htmlFor="description">Hotel Description:</label>
-            <input
-                type="text"
-                id="description"
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-            />
-        </div>
+            <div className='hotel-form-input'>
+                <label htmlFor="description">Hotel Description:</label>
+                <input
+                    type="text"
+                    id="description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </div>
 
-        <div className='hotel-form-input'>
-            <label htmlFor="rating">Hotel Rating:</label>
-            <input
-                type="text"
-                id="rating"
-                value={rating}
-                onChange={(e) => setRating(e.target.value)}
-            />
-        </div>
+            <div className='hotel-form-input'>
+                <label htmlFor="rating">Hotel Rating:</label>
+                <input
+                    type="text"
+                    id="rating"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                />
+            </div>
 
-        <div className='hotel-form-input'>
-            <label htmlFor="images">Images:</label>
-            <input type="file" id="images" multiple onChange={handleImageChange} />
-            {images.map((image, index) => (
-                <p key={index}>{image.name}</p>
-            ))}
-        </div>
-        <button type="submit">Submit</button>
-    </form>
-);
+            <div className='hotel-form-input'>
+                <label htmlFor="images">Images:</label>
+                <input type="file" id="images" multiple onChange={handleImageChange} />
+                {images.map((image, index) => (
+                    <p key={index}>{image.name}</p>
+                ))}
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default HotelForm;
