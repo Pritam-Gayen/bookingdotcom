d850f14b2b88b3cc874b76d0cddd3b0d33f57bda
Pritam <pritsaccount@gmail.com>
2025-04-25 17:07:37 +0530

    MODIFIED FOR REGISTER CLIENT

--- Client/src/login.js
+++ Client/src/login.js
@@ -17,6 +17,9 @@ const LoginButton = ({ setuserEmail, setloginState, setUserPhoto, setUserName, s
 
   const [message, setMessage] = useState("");
 
+  const [showVerification, setShowVerification] = useState(false);
+  const [verificationCode, setVerificationCode] = useState("");
+
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -33,6 +36,21 @@ const LoginButton = ({ setuserEmail, setloginState, setUserPhoto, setUserName, s
     }
   };
 
+
+  const handleVerify = async () => {
+    try {
+      const response = await axios.post("https://bookingdotcom-gkgr.onrender.com/verify", {
+        email: formData.email,
+        verificationCode,
+      });
+      setMessage(response.data.message);
+      setShowVerification(false);
+    } catch (error) {
+      setMessage(error.response?.data?.message || "Verification failed!");
+    }
+  };
+
+  
   return (
     <div class="d-flex justify-content-center align-items-center" style={{ marginTop: '100px', marginBottom: '100px' }}>
       <form onSubmit={handleSubmit}>
@@ -42,7 +60,19 @@ const LoginButton = ({ setuserEmail, setloginState, setUserPhoto, setUserName, s
         <button type="submit">Send Verification Code</button>
         <p>{message}</p>
       </form>
+    
 
+    {showVerification && (
+        <div style={{ marginTop: "20px" }}>
+          <input
+            type="text"
+            placeholder="Enter Verification Code"
+            value={verificationCode}
+            onChange={(e) => setVerificationCode(e.target.value)}
+          />
+          <button onClick={handleVerify}>Check Verification Code</button>
+        </div>
+      )}
     </div>
   );
 };
